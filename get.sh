0692d1fd2e3c5f1926987f6d29d89006f57afbf2
hunter.lippmann.com <hunter.lippmann.com>
2021-04-19 21:10:05 -0400

    add get.sh, files/, log/

---
+++ get.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+mkdir -p files log
+url="https://www.equityapartments.com/washington-dc/gallery-place-mt-vernon-triangle/425-mass-apartments"
+now=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+
+wget $url -O files/$now.html -o log/$now.log
+
