3a40eca716d5bb80ef075d8b18615d316862e1e1
hunter.lippmann.com <hunter.lippmann.com>
2021-04-19 21:47:01 -0400

    add save to ia

--- get.sh
+++ get.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 mkdir -p files log
 url="https://www.equityapartments.com/washington-dc/gallery-place-mt-vernon-triangle/425-mass-apartments"
+ia="https://web.archive.org/save"
 now=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 
 # wget $url -O files/$now.html -o log/$now.log
 wget $url -O file.html
-
+curl -s $ia/$url > /dev/null
