37ea430af330686fc8ac5bac44830e527a49690d
hunter.lippmann.com <hunter.lippmann.com>
2021-04-19 21:36:12 -0400

    changed cron to 7am (11 UTC) and 8pm (0 UTC)

--- get.sh
+++ get.sh
@@ -3,5 +3,6 @@ mkdir -p files log
 url="https://www.equityapartments.com/washington-dc/gallery-place-mt-vernon-triangle/425-mass-apartments"
 now=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 
-wget $url -O files/$now.html -o log/$now.log
+# wget $url -O files/$now.html -o log/$now.log
+wget $url -O file.html
 
